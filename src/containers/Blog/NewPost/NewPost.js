f285c6090c5c419537de9888c0372da301426541
Jonel Cabotaje <=lejondy@gmail.com>
2019-01-12 19:29:02 +0800

    react router implementation

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -6,21 +6,24 @@ import './NewPost.css';
 class NewPost extends Component {
     state = {
         title: '',
-        body: '',
+        content: '',
         author: 'Max'
     }
-    
-    postDataHandler = () => {
-    	const data = {
-    		title: this.state.title,
-    		body: this.state.body,
-    		author: this.state.author
-    	};
 
-    	axios.post('https://jsonplaceholder.typicode.com/posts',data )
-    	.then(response => {
-    		console.log(response);
-    	})
+    componentDidMount () {
+        
+    }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        };
+        axios.post('/posts', data)
+            .then(response => {
+                console.log(response);
+            });
     }
 
     render () {
@@ -30,7 +33,7 @@ class NewPost extends Component {
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.body} onChange={(event) => this.setState({body: event.target.value})} />
+                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
